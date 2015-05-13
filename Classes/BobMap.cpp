d16c37b107124ee3bd73356d3a9671885da93301
ctxdegitlab <842723654@qq.com>
2015-05-13 19:59:40 +0800

    add new genetic algorithm

---
+++ Classes/BobMap.cpp
@@ -0,0 +1,9 @@
+//
+//  BobMap.cpp
+//  LearnGA
+//
+//  Created by DONG on 15/5/13.
+//
+//
+
+#include "BobMap.h"
---
+++ Classes/BobMap.h
@@ -0,0 +1,14 @@
+//
+//  BobMap.h
+//  LearnGA
+//
+//  Created by DONG on 15/5/13.
+//
+//
+
+#ifndef __LearnGA__BobMap__
+#define __LearnGA__BobMap__
+
+#include <stdio.h>
+
+#endif /* defined(__LearnGA__BobMap__) */
